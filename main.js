32c88d0c6be955556a033bbea5786da2522b9513
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2025-02-18 15:48:08 +0200

    Update main.js
    Reverted to original

--- main.js
+++ main.js
@@ -1,53 +1,24 @@
-body {
-  max-width: 1200px;
-  margin: 2em auto;
-  main {
-    max-width: 80%;
-    margin: 2em auto;
-  }
-}
+import './style.scss'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-// @mixin message($col: white, $bor: 1px solid #ddd, $br: 5px, $fs: 1rem, $pd: 1rem 1.25rem, $m: 1em 0, $disp: inline-block, $bg: #888, $cur: pointer) {
-//   border: $bor;
-//   color: $col;
-//   border-radius: $br;
-//   font-size: $fs;
-//   padding: $pd;
-//   margin: $m;
-//   display: $disp;
-//   background: $bg;
-//   cursor: $cur;
-// }
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-@mixin message($col, $bor, $br, $fs, $pd, $m, $disp, $cur, $bg: #888) {
-  border: $bor;
-  color: $col;
-  border-radius: $br;
-  font-size: $fs;
-  padding: $pd;
-  margin: $m;
-  display: $disp;
-  background: $bg;
-  cursor: $cur;
-}
-
-.msg {
-  @include message($col: white, $bor: 1px solid #ddd, $br: 5px, $fs: 1rem, $pd: 1rem 1.25rem, $m: 1em 0, $disp: inline-block, $cur: pointer, $bg);
-
-  &:hover {
-    background: #111111;
-  }
-
-  &-info {
-    @extend .msg;
-    background: #7ec906;
-
-    &:hover {
-      background: #487001;
-    }
-  }
-  h1 {
-    margin: 0 0 0.2em 0;
-  }
-}
-
+setupCounter(document.querySelector('#counter'))
